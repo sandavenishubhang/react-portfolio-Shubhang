72bc656e43303d3b715fb5a0c5f973fb2d141743
Shubh <shubhang@Shubhs-MacBook-Air.local>
2025-01-21 17:16:14 -0500

    Updated Contact component for better structure and styling

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,35 +14,14 @@ const Contact = () => {
         Get in Touch
       </motion.h1>
 
-      {/* Contact Information */}
-      <div className="text-center tracking-tighter">
-        {/* Address */}
-        <motion.p
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
-          className="my-4"
-        >
-          {CONTACT.address}
-        </motion.p>
-
-        {/* Phone Number */}
-        <motion.p
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 1 }}
-          className="my-4"
-        >
-          {CONTACT.phoneNo}
-        </motion.p>
-
-        {/* Email with Link */}
+      {/* Email Section */}
+      <div className="text-center">
         <motion.a
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: -50 }}
           transition={{ duration: 0.8 }}
           href={`mailto:${CONTACT.email}`}
-          className="border-b text-purple-500 hover:text-purple-700 transition duration-300"
+          className="text-purple-500 border-b hover:text-purple-700 transition duration-300"
         >
           {CONTACT.email}
         </motion.a>

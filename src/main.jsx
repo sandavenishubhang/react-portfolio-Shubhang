29b8875f4aab40d27fd4bc89615bc5fa72d0e986
Shubh <shubhang@Shubhs-MacBook-Air.local>
2025-01-21 16:48:05 -0500

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
